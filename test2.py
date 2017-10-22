6983902aef8b0f3fc9133cdbb042e72ce73c8599
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2017-10-22 18:05:01 +0200

    Using pybittrex library. Printing data from Public APIs

--- test2.py
+++ test2.py
@@ -7,126 +7,78 @@ import requests
 import time
 from pybittrex.auth import BittrexAuth
 c = Client(api_key=config.key, api_secret=config.secret)
+api = Bittrex(config.key, config.secret)
 market = "BTC-VTC"
+vtc = 3
+currency = "VTC"
 
-class Client(object):
-    """Create a new session to the Bittrex exchange."""
-    def __init__(self, api_key, api_secret):
-        self.api_version = 'v1.1'
-        self.api_base = 'https://bittrex.com/api/%s' % (self.api_version)
-
-        self.api_key = api_key
-        self.api_secret = api_secret
-
-        # create a session object
-        self.session = requests.Session()
-
-    def get_api_version(self):
-        return self.api_version
-
-    def _get_nonce(self):
-        """Authenticated requests all require a nonce."""
-        return str(round(time.time()))
-
-    def _build_url(self, endpoint):
-        """Helper function to build the full URL."""
-        return self.api_base + endpoint
-
-    def _call(self, url, params=None):
-        """Call the API """
-
-        if not params:
-            # for authenticated requests with no parameters
-            params = {}
-
-        # figure out if we need to authenticate or not
-        if 'public' in url:
-            auth = None
-        else:
-            auth = BittrexAuth(self.api_secret)
-            params['apikey'] = self.api_key
-            params['nonce'] = self._get_nonce()
-
-        return self.session.get(url, params=params, auth=auth)
-
-                                    # Public API
-    # ----------
-
-    def get_markets(self):
-        """Used to get the open and available trading markets at Bittrex along with other metadata."""
-        url = self._build_url('/public/getmarkets')
-
-        return self._call(url)
-
-    markets = c.get_markets().json()['result']
+#Used to get the open and available trading markets at Bittrex along with other metadata.
+markets = c.get_markets().json()['result']
 #For specific currency  markets[0] index means the number of currency pair
-    print (markets[2])['MarketName']
+market = (markets[2])['MarketName']
+print "We are working on following market: {0} ".format(market)
     #market = (markets[2])['MarketName']
 #For all markets
 #for i in markets:
 #    print(i['MarketName'])
-    def get_currencies(self):
-        """Used to get all supported currencies at Bittrex along with other metadata."""
-        url = self._build_url('/public/getcurrencies')
 
-        return self._call(url)
-    currencies = c.get_currencies().json()['result']
-    print (currencies[3])['Currency']
+#Used to get all supported currencies at Bittrex along with other metadata
+currencies = c.get_currencies().json()['result']
+print "We selected this currency: {0}".format((currencies[vtc])['Currency'])
 
+#Used to get the current tick values for a market.
+ticker = c.get_ticker(market).json()['result']
+print "The current Ask is: {0}".format((ticker)['Ask'])
+print "The current Bid is: {0}".format((ticker)['Bid'])
 
-    def get_ticker(self, market, *args, **kwargs):
-        """Used to get the current tick values for a market."""
-        url = self._build_url('/public/getticker')
-
-        payload = {'market': market}
-
-        return self._call(url, params=payload)
-    ticker = c.get_ticker(market).json()['result']
-    print (ticker)['Ask']
-    print (ticker)['Bid']
+#Used to get the last 24 hour summary of all active exchanges.
+market_summ = c.get_market_summaries().json()['result']
+market_summ_vtc = market_summ[175]
+print "The current bought volume is {0}".format(market_summ_vtc['Volume'])
+# The BID for volume
+# print market_summ_vtc['Bid']
 
 
+#Used to get retrieve the orderbook for a given market.
+sellorderbook = c.get_orderbook(market, 'sell').json()['result']
+buyorderbook = c.get_orderbook(market, 'buy').json()['result']
 
-    def get_market_summaries(self):
-        """Used to get the last 24 hour summary of all active exchanges."""
-        url = self._build_url('/public/getmarketsummaries')
+# show 2 last  sell orders
+items = (itemgetter(0, 1)(sellorderbook))
+for i in items:
+    print "One of the last SELL orders {0}".format(i['Quantity'])
 
-        return self._call(url)
-    market_summ = c.get_market_summaries().json()['result']
-    market_summ_vtc = market_summ[175]
-    print market_summ_vtc['Volume']
-    print market_summ_vtc['Bid']
-
-
-
-
-#Not used
-   # def get_market_summary(self, market, *args, **kwargs):
-    #    """Used to get the last 24 hour summary of all active exchanges."""
-     #   url = self._build_url('/public/getmarketsummary')
-#
- #       payload = {'market': market}
-
-#        return self._call(url, params=payload)
- #   market_summary = c.get_market_summary(market).json()['result']
-  #  print market_summary
+# show 2 last buy orders
+items = (itemgetter(0, 1)(buyorderbook))
+for b in items:
+    print "One of the last BUY orders {0}".format(b['Quantity'])
 
 
 
-    def get_orderbook(self, market, type, *args, **kwargs):
-        """Used to get retrieve the orderbook for a given market."""
-        url = self._build_url('/public/getorderbook')
-
-        payload = {'market': market, 'type': type}
-
-        return self._call(url, params=payload)
-
+markethistory = c.get_market_history(market).json()['result']
+#print markethistory
+# show 2 last  sell orders from history
+items = (itemgetter(2, 3)(markethistory))
+for i in items:
+  print "The last 2 SELL orders from history {0}".format(i['Total'])
 
 
-    def get_market_history(self, market, *args, **kwargs):
-        """Used to retrieve the latest trades that have occured for a specific market."""
-        url = self._build_url('/public/getmarkethistory')
+# Account API
+#Used to retrieve balances from your account.
+balances = c.get_balances().json()['result']
+items = (itemgetter(0, 1)(balances))
+for i in balances:
+    print "Balance for {0} is:".format(i['Currency'])
+    print (i['Balance'])
 
-        payload = {'market': market}
 
-        return self._call(url, params=payload)
+
+
+
+
+
+
+
+
+
+
