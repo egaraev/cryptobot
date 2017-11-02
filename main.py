b80c2892cef642b7a4be89fa4d62d7748fce892a
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2017-11-02 23:11:47 +0100

    Ver 1.4 Stop loss function added. List of currencies updated 2 indicator using

--- main.py
+++ main.py
@@ -8,36 +8,30 @@ import requests
 import time
 import yaml
 from pybittrex.auth import BittrexAuth
-#Configuring bytrex client
-c = Client(api_key=config.key, api_secret=config.secret)
+
+c = Client(api_key=config.key, api_secret=config.secret)   #Configuring bytrex client with API key/secret from config file
 
 #Opening config file with variables
 
-#Open config file
-with open("variables.yml", "r") as variables_file:
+with open("variables.yml", "r") as variables_file:   #Open config file
     variables = yaml.load(variables_file)
-#The size for opening orders
-    buy_size = float(variables['buy_order_size'])
-#Minimal size for closing oders
-    sell_size = float(variables['min_sell_order_size'])
-#The size of profit
-    profit = float(variables['multiplier'])
-#The main currency
-    currency_btc = variables['currency_btc']
-#The maximum quantity of open orders at the same time
-    max = float(variables['maxorder'])
+    buy_size = float(variables['buy_order_size']) #The size for opening orders
+    sell_size = float(variables['min_sell_order_size'])  #Minimal size for closing oders
+    profit = float(variables['multiplier'])  #The size of profit we want to take
+    currency_btc = variables['currency_btc']  #The main currency
+    max = float(variables['maxorder'])  #The maximum quantity of open orders at the same time
 
-
+#The lit of available markets for trading with max order sizes (order sizes from history, which we can count as BIG)
+available_markets = [{"MarketName": 'BTC-QTUM', "BuyOrders": 1000, "SellOrders": 1000}, {"MarketName": 'BTC-ETH', "BuyOrders": 30, "SellOrders": 30},
+                     {"MarketName": 'BTC-LTC', "BuyOrders": 100, "SellOrders": 100}, {"MarketName": 'BTC-VTC', "BuyOrders": 1000, "SellOrders": 1000},
+                     {"MarketName": 'BTC-XMR', "BuyOrders": 100, "SellOrders": 100}, {"MarketName": 'BTC-XRP', "BuyOrders": 3000, "SellOrders": 3000},
+                     {"MarketName": 'BTC-ZEC', "BuyOrders": 50, "SellOrders": 50}, {"MarketName": 'BTC-ETC', "BuyOrders": 600, "SellOrders": 600}]
 
 #Setup tick interval
 TICK_INTERVAL = 60  # seconds
 
 #Get the market summaries
 market_summ = c.get_market_summaries().json()['result']
-
-
-#bigorders = float(variables['markets']['btc_qtum']['bigorders'])
-
 
 
 #The main function
@@ -59,49 +53,34 @@ def main():
 #what will be done every loop iteration
 def tick():
 
-
-    for summary in market_summ:
-        if market_list(summary['MarketName']):
+    for summary in market_summ: #Loop trough the market summary
+        if market_list(summary['MarketName']):  #Check if currency is from my allowed list
             market = summary['MarketName']
-            buyorders = buysellorders(market,'BuyOrders')
-            sellorders = buysellorders(market,'SellOrders')
-            day_close = summary['PrevDay']
-            last = float(summary['Last'])
-
-            buyorderbook = c.get_orderbook(market, 'buy').json()['result'][:30]
+            buyorders = buysellorders(market,'BuyOrders') #Get the size of big buy orders from history
+            sellorders = buysellorders(market,'SellOrders')  #Get the size of big sell orders from history
+            buyorderbook = c.get_orderbook(market, 'buy').json()['result'][:50]  #getting buy orders history last 50 orders
             buycount = 0
-            for buyorder in buyorderbook:
+            for buyorder in buyorderbook:  #Counting how much big buy orders we have in history
                 buyamount = buyorder['Quantity']
                 if buyamount >= buyorders:
-                    buycount +=1
+                    buycount += 1
+            buycountresult = buycount
 
-            sellorderbook = c.get_orderbook(market, 'sell').json()['result'][:30]
+            sellorderbook = c.get_orderbook(market, 'sell').json()['result'][:50]  #getting sell orders history last 50 orders
             sellcount = 0
-            for sellorder in sellorderbook:
+            for sellorder in sellorderbook:   #Counting how much big buy orders we have in history
                 sellamount = sellorder['Quantity']
                 if sellamount >= sellorders:
                     sellcount += 1
+            sellcountresult = sellcount
 
 
 
-
-
-
-
-
-
-
-###############################################################################################################
-    for summary in market_summ:
-        if (summary['MarketName'][0:3]) == currency_btc:
-        #if market_list(summary['MarketName']):
-            market = summary['MarketName']
-            #print c.get_orderbook(market, 'sell').json()['result']
-            day_close = summary['PrevDay']
-        #Current price
-            last = float(summary['Last'])
-            bid = float(summary['Bid'])
-            ask = float(summary['Ask'])
+            day_close = summary['PrevDay']   #Getting day of closing order
+        #Current prices
+            last = float(summary['Last'])  #last price
+            bid = float(summary['Bid'])    #sell price
+            ask = float(summary['Ask'])    #buy price
         #How much market has been changed
             percent_chg = ((last / day_close) - 1) * 100
         #HOW MUCH TO BUY
@@ -112,13 +91,16 @@ def tick():
             bought_quantity = get_closed_orders(market, 'Quantity')
             sell_quantity = bought_quantity
 
+            #print market, buycountresult, sellcountresult
 
 
-#If the price for some currency rapidly increased from 30% till 50%. lets buy something
-            if 1 < percent_chg < 50:
+
+#################################BUYING ALGORITHM#####################
+#If the price for some currency rapidly increased from 25% till 50% OR we have more then 10 big BUY orders on the market, let`s buy something too
+            if 25 < percent_chg < 50 or buycountresult >= 10:
                 balance_res = get_balance_from_market(market)
                 current_balance = balance_res['result']['Available']
-            # Check if we have open orders
+            # Check if we have open orders or some unsold currency
                 if has_open_order(market, 'LIMIT_BUY') or current_balance is not None:
                     print('Order already opened to buy  ' + market)
                 else:
@@ -127,11 +109,28 @@ def tick():
 #########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
                 #print c.buy_limit(market, buy_quantity, last).json()
 #########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
+#If we have twice more BIG buy orders then BIG sell Orders, it means that price is growing, Let` buy something
 
+            elif buycountresult > sellcountresult*2 :
+                balance_res = get_balance_from_market(market)
+                current_balance = balance_res['result']['Available']
+                # Check if we have open orders or some unsold currency
+                if has_open_order(market, 'LIMIT_BUY') or current_balance is not None:
+                    print('Order already opened to buy  ' + market)
+                else:
+                    # Buy some currency
+                    print('Purchasing ' + str(format_float(buy_quantity)) + ' units of ' + market + ' for ' + str(format_float(ask)))
+#########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
+                    # print c.buy_limit(market, buy_quantity, last).json()
+#########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
+
+
+#################################SELLING ALGORITHM#####################
 #Check if we have this currency for sell
             if bought_price != None:
 
-#If  we got our profit, lets sell
+#If  we got our profit, lets sell this shit
+## "TAKE PROFIT" MECHANIZM - we can take our percent from profit variable and sell currency
                 if last >= bought_price*profit:
                     balance_res = get_balance_from_market(market)
                     current_balance = balance_res['result']['Available']
@@ -141,7 +140,7 @@ def tick():
                     if current_balance is None:
                         pass
 
-#If curent balance more then zero
+#If curent balance of this currency more then zero
                     elif current_balance > 0:
                     # Lets Sell some
                         if has_open_order(market, 'LIMIT_SELL'):
@@ -153,20 +152,24 @@ def tick():
 #########!!!!!!!!! SELLING MECHANIZM, DANGER !!!!###################################
                     else:
                         pass
-                    #print('Not enough ' + market + ' to open a sell order')
+
+#"STOP LOSS" MECHANIZM. WE should sell failed currency before price goes down and reach min selling limit. If sell now we are losing 50%. If not - we will lose 100% of order`s cost
+                elif last < bought_price and sell_quantity*last >= sell_size:
+                    print ('Selling ' + str(format_float(sell_quantity)) + ' units of ' + market + ' for ' + str(format_float(bid)) + '  and losing  - ' + str(format_float(bid-bought_price)) + ' BTC')
+#########!!!!!!!!! SELLING MECHANIZM, DANGER !!!!###################################
+                #   print c.sell_limit(market, sell_quantity, last).json()
+#########!!!!!!!!! SELLING MECHANIZM, DANGER !!!!###################################
             else:
                 pass
 
 
-
-
-
+### FUNCTIONS
 ###############################################################################################################
 #Analize of last buy orders
 
 #Allowed currencies function
 def market_list(marketname):
-    allowed_markets = [{"MarketName": 'BTC-QTUM', "BuyOrders": 10, "SellOrders": 20}, {"MarketName": 'BTC-ETH', "BuyOrders": 35, "SellOrders": 50}]
+    allowed_markets = available_markets
     for markets in allowed_markets:
         if markets['MarketName'] == marketname:
             return True
@@ -175,9 +178,9 @@ def market_list(marketname):
 
 
 
-
+#function for getting "Big" values for sell and buy for each market
 def buysellorders(marketname, value):
-    allowed_markets = [{"MarketName": 'BTC-QTUM', "BuyOrders": 100, "SellOrders": 100}, {"MarketName": 'BTC-ETH', "BuyOrders": 10, "SellOrders": 10}]
+    allowed_markets = available_markets
     for markets in allowed_markets:
         if markets['MarketName'] == marketname:
             return markets[value]
