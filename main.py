14f9a9fb94993b0c24759312cb2bdf50651434f3
egaraev@gmail.com <Kax0vka44609>
2017-11-04 18:12:14 +0100

    Ver 1.6. Candlestick analize added from API v2.0. Also fixed few bugs with selling

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from pybittrex.client import Client
 import requests
 import time
 import yaml
+import hmac
+import hashlib
 from pybittrex.auth import BittrexAuth
 
 c = Client(api_key=config.key, api_secret=config.secret)   #Configuring bytrex client with API key/secret from config file
@@ -25,7 +27,10 @@ with open("variables.yml", "r") as variables_file:   #Open config file
 available_markets = [{"MarketName": 'BTC-QTUM', "BuyOrders": 1000, "SellOrders": 1000}, {"MarketName": 'BTC-ETH', "BuyOrders": 30, "SellOrders": 30},
                      {"MarketName": 'BTC-LTC', "BuyOrders": 100, "SellOrders": 100}, {"MarketName": 'BTC-VTC', "BuyOrders": 1000, "SellOrders": 1000},
                      {"MarketName": 'BTC-XMR', "BuyOrders": 100, "SellOrders": 100}, {"MarketName": 'BTC-XRP', "BuyOrders": 3000, "SellOrders": 3000},
-                     {"MarketName": 'BTC-ZEC', "BuyOrders": 50, "SellOrders": 50}, {"MarketName": 'BTC-ETC', "BuyOrders": 600, "SellOrders": 600}]
+                     {"MarketName": 'BTC-ZEC', "BuyOrders": 50, "SellOrders": 50}, {"MarketName": 'BTC-ETC', "BuyOrders": 600, "SellOrders": 600},
+                     {"MarketName": 'BTC-DASH', "BuyOrders": 20, "SellOrders": 20}, {"MarketName": 'BTC-NEO', "BuyOrders": 400, "SellOrders": 400},
+                     {"MarketName": 'BTC-XLM', "BuyOrders": 1000000, "SellOrders": 1000000}, {"MarketName": 'BTC-OMG', "BuyOrders": 1000, "SellOrders": 1000},
+                     {"MarketName": 'BTC-BCC', "BuyOrders": 15, "SellOrders": 15}, {"MarketName": 'BTC-LSK', "BuyOrders": 1000, "SellOrders": 1000}]
 
 #Setup tick interval
 TICK_INTERVAL = 60  # seconds
@@ -79,9 +84,19 @@ def tick():
                     sellsum = sellamount + sellsum
             selltotalsumm = sellsum  #total summ of SELL orders on the market
             sellcountresult = sellcount
+            #Candle analisys
+            lastcandle = get_candles(market, 'thirtymin')['result'][-1:]
+            low = float(lastcandle[0]['L'])
+            open = float(lastcandle[0]['O'])
+            close = float(lastcandle[0]['C'])
+            previouscandle = get_candles(market, 'thirtymin')['result'][-2:]
+            prevlow = float(previouscandle[0]['L'])
+            prevopen = float(previouscandle[0]['O'])
+            prevclose = float(previouscandle[0]['C'])
 
+###################
 
-
+####################
             day_close = summary['PrevDay']   #Getting day of closing order
         #Current prices
             last = float(summary['Last'])  #last price
@@ -101,88 +116,105 @@ def tick():
 
 
 
-#################################BUYING ALGORITHM#####################
-#If the price for some currency rapidly increased from 25% till 50% OR we have more then 10 big BUY orders on the market, let`s buy something too
-            if 25 < percent_chg < 50 or buycountresult >= 10:
+                        #################################BUYING ALGORITHM#####################
+            ###################################################################################################
+#If the price for some currency rapidly increased from 25% till 50%  let`s buy something too
+            if 25 < percent_chg < 50:
                 balance_res = get_balance_from_market(market)
                 current_balance = balance_res['result']['Available']
+                #print market, current_balance
             # Check if we have open orders or some unsold currency
-                if has_open_order(market, 'LIMIT_BUY') or current_balance is not None:
+                if has_open_order(market, 'LIMIT_BUY'):
                     print('Order already opened to buy  ' + market)
+                elif current_balance is not None and current_balance != 0.0:
+                    print('We already have ' + str(format_float(current_balance)) + '  ' + market +  ' on our balance')
                 else:
                 #Buy some currency
-                    print('Purchasing ' + str(format_float(buy_quantity)) +' units of ' + market + ' for ' + str(format_float(ask)))
+                    print('Purchasing ' + str(format_float(buy_quantity)) +' units of ' + market + ' for ' + str(format_float(bid)))
 #########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
                 #print c.buy_limit(market, buy_quantity, last).json()
 #########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
-#If we have more BIG buy orders then BIG sell Orders, and volume of BUY order is bigger then volume pf sell prders, it means that price is growing, Let` buy something
+#If we have twice more BIG buy orders then BIG sell Orders, and volume of BUY order is twice bigger then volume pf sell prders, it means that price is growing, Let` buy something
 
-            elif buytotalsumm > selltotalsumm and buycountresult > sellcountresult:
+            elif buytotalsumm > selltotalsumm*2 and buycountresult > sellcountresult*2:
                 balance_res = get_balance_from_market(market)
                 current_balance = balance_res['result']['Available']
                 # Check if we have open orders or some unsold currency
-                if has_open_order(market, 'LIMIT_BUY') or current_balance is not None:
+                if has_open_order(market, 'LIMIT_BUY'):
                     print('Order already opened to buy  ' + market)
+                elif current_balance is not None and current_balance != 0.0:
+                    print('We already have ' + str(format_float(current_balance)) + '  ' + market +  ' on our balance')
                 else:
                     # Buy some currency
-                    print('Purchasing ' + str(format_float(buy_quantity)) + ' units of ' + market + ' for ' + str(format_float(ask)))
+                    print('Purchasing ' + str(format_float(buy_quantity)) + ' units of ' + market + ' for ' + str(format_float(bid)))
 #########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
                     # print c.buy_limit(market, buy_quantity, last).json()
 #########!!!!!!!!! BUYING MECHANIZM, DANGER !!!!###################################
 
 
-#################################SELLING ALGORITHM#####################
+
+
+
+                        #################################SELLING ALGORITHM#####################
+                    ################################################################################
 #Check if we have this currency for sell
             if bought_price != None:
-
-#If  we got our profit, lets sell this shit
-## "TAKE PROFIT" MECHANIZM - we can take our percent from profit variable and sell currency
-                if last >= bought_price*profit:
-                    balance_res = get_balance_from_market(market)
-                    current_balance = balance_res['result']['Available']
-
-
-#check current balance
-                    if current_balance is None:
+                balance_res = get_balance_from_market(market)
+                current_balance = balance_res['result']['Available']
+                # check current balance
+                if current_balance is None:
+                    pass
+                    # If curent balance of this currency more then zero
+                elif current_balance > 0:
+                ##Check if we have completelly green candle
+                    if open == low and prevclose <= open:
+                        print (" We have GREEN candle for " + market + " and it is better to wait, before sell")
                         pass
 
-#If curent balance of this currency more then zero
-                    elif current_balance > 0:
+                    elif open == prevclose and last > bought_price*profit:
+                        print "We have good trend for " + market
+                        pass
+
+                    else:
+#If  we got our profit, lets sell this shitcoin
+## "TAKE PROFIT" MECHANIZM - we can take our percent from profit variable and sell currency
+                        if last >= bought_price*profit:
                     # Lets Sell some
-                        if has_open_order(market, 'LIMIT_SELL'):
-                            print('Order already opened to sell  ' + market)
-                        else:
-                            print('Selling ' + str(format_float(sell_quantity)) + ' units of ' + market + ' for ' + str(format_float(bid)) + '  and getting  +' + str(format_float(bid-bought_price)) + ' BTC')
+                            if has_open_order(market, 'LIMIT_SELL'):
+                                print('Order already opened to sell  ' + market)
+                            else:
+                                print('Selling ' + str(format_float(sell_quantity)) + ' units of ' + market + ' for ' + str(format_float(ask)) + '  and getting  +' + str(format_float(ask-bought_price)) + ' BTC')
 #########!!!!!!!!! SELLING MECHANIZM, DANGER !!!!###################################
  #                      print c.sell_limit(market, sell_quantity, last).json()
 #########!!!!!!!!! SELLING MECHANIZM, DANGER !!!!###################################
-                    else:
-                        pass
+
 
 #"STOP LOSS" MECHANIZM. WE should sell failed currency before price goes down and reach min selling limit. If sell now we are losing 50%. If not - we will lose 100% of order`s cost
-                elif last < bought_price and sell_quantity*last >= sell_size:
-                    balance_res = get_balance_from_market(market)
-                    current_balance = balance_res['result']['Available']
-                    # check current balance
-                    if current_balance is None:
-                        pass
-                        # If curent balance of this currency more then zero
-                    elif current_balance > 0:
+                        elif last < bought_price and sell_size <= sell_quantity*last:
                         # Lets Sell some
-                        if has_open_order(market, 'LIMIT_SELL'):
-                            print('Order already opened to sell  ' + market)
-                        else:
-                            print ('Selling ' + str(format_float(sell_quantity)) + ' units of ' + market + ' for ' + str(format_float(bid)) + '  and losing  - ' + str(format_float(bid-bought_price)) + ' BTC')
+                            if has_open_order(market, 'LIMIT_SELL'):
+                                print('Order already opened to sell  ' + market)
+                            else:
+                                print ('Selling ' + str(format_float(sell_quantity)) + ' units of ' + market + ' for ' + str(format_float(ask)) + '  and losing  - ' + str(format_float(ask-bought_price)) + ' BTC')
 #########!!!!!!!!! SELLING MECHANIZM, DANGER !!!!###################################
                 #   print c.sell_limit(market, sell_quantity, last).json()
 #########!!!!!!!!! SELLING MECHANIZM, DANGER !!!!###################################
+                else:
+                    pass
             else:
                 pass
+
+
+
+
 
 
 ### FUNCTIONS
 ###############################################################################################################
 #Analize of last buy orders
+
+
+
 
 #Allowed currencies function
 def market_list(marketname):
@@ -254,6 +286,28 @@ def has_open_order(market, order_type):
             return True
     return False
 
+
+
+#def get_candles(self, market, tick_interval):
+#    return self._api_query(path_dict={
+#        'v2.0': '/pub/market/GetTicks'
+#    }, options={
+#        'marketName': market, 'tickInterval': tick_interval
+#    }, protection='pub')
+
+
+def get_candles(market, tick_interval):
+    url = 'https://bittrex.com/api/v2.0/pub/market/GetTicks?apikey=' + config.key + '&MarketName=' + market +'&tickInterval=' + str(tick_interval)
+    return signed_request(url)
+
+
+def signed_request(url):
+    now = time.time()
+    url += '&nonce=' + str(now)
+    signed = hmac.new(config.secret, url.encode('utf-8'), hashlib.sha512).hexdigest()
+    headers = {'apisign': signed}
+    r = requests.get(url, headers=headers)
+    return r.json()
 
 
 
