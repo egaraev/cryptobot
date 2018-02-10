ba01f7f821648409ebb4fe236fa061a1bc43fa37
egaraev@gmail.com <Kax0vka44609>
2018-02-10 14:05:39 +0100

    Ver 1.99  Authentication done. Before adding foreign keys to DB

--- webinterface/server.py
+++ webinterface/server.py
@@ -1,46 +1,113 @@
-from flask import Flask, flash, render_template, redirect, url_for, request, session
+from flask import Flask, flash, render_template, redirect, url_for, request, session, escape
 from module.database import Database
-
+import MySQLdb
 
 app = Flask(__name__)
 app.secret_key = "mys3cr3tk3y"
 db = Database()
 
+db1 = MySQLdb.connect(host="localhost", user="cryptouser", passwd="123456", db="cryptodb")
+cur = db1.cursor()
+
+
+class ServerError(Exception):pass
+
 @app.route('/')
 def index():
-    data = db.read_settings(None)
-    
-    return render_template('index.html')
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('index.html', session_user_name=username_session)
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username_form  = request.form['username']
+        password_form  = request.form['password']
+        cur.execute("SELECT COUNT(1) FROM users WHERE username = %s;", [username_form]) # CHECKS IF USERNAME EXSIST
+        if cur.fetchone()[0]:
+            cur.execute("SELECT password FROM users WHERE username = %s;", [username_form]) # FETCH THE HASHED PASSWORD
+            for row in cur.fetchall():
+                if password_form == row[0]:
+                    session['username'] = request.form['username']
+                    return redirect(url_for('index'))
+                else:
+                    error = "Invalid Credential"
+        else:
+            error = "Invalid Credential"
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 @app.route('/add/')
 def add():
-    return render_template('/add.html')
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('add.html', session_user_name=username_session)
+    return redirect(url_for('login'))
 
 
 @app.route('/adduser', methods = ['POST', 'GET'])
 def adduser():
-    if request.method == 'POST' and request.form['save']:
-        if db.insert(request.form):
-            flash("A new user has been added")
+        if request.method == 'POST' and request.form['save']:
+            if db.insert(request.form):
+                flash("A new user has been added")
+            else:
+                flash("A new user can not be added")
+
+            return redirect(url_for('users'))
         else:
-            flash("A new user can not be added")
-            
-        return redirect(url_for('users'))
-    else:
-        return redirect(url_for('users'))
+            return redirect(url_for('users'))
+
+
+
+
+
+
+#    if 'username' in session:
+#        username_session = escape(session['username']).capitalize()
+#        #return render_template('index.html', session_user_name=username_session)
+#        if request.method == 'POST' and request.form['save']:
+#            if db.insert(request.form):
+#                flash("A new user has been added")
+#            else:
+#                flash("A new user can not be added")
+
+#            return redirect(url_for('users'))
+#        else:
+#            return redirect(url_for('users'))
+
+#    return redirect(url_for('login'))
+
+
+
 
 
 
 @app.route('/delete/<int:id>/')
 def delete(id):
     data = db.read(id)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        if len(data) == 0:
+            return redirect(url_for('users'))
+        else:
+            session['delete'] = id
+        return render_template('delete.html', data = data, session_user_name=username_session)
 
-    if len(data) == 0:
-        return redirect(url_for('users'))
-    else:
-        session['delete'] = id
-        return render_template('delete.html', data = data)
+    return redirect(url_for('login'))
+
+
+
 
 @app.route('/deleteuser', methods = ['POST'])
 def deleteuser():
@@ -63,19 +130,25 @@ def deleteuser():
 @app.route('/users')
 def users():
     data = db.read(None)
-
-    return render_template('/index_u.html', data = data)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('/index_u.html', session_user_name=username_session, data = data)
+    return redirect(url_for('login'))
 
 
 @app.route('/update/<int:id>/')
 def update(id):
     data = db.read(id)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
 
-    if len(data) == 0:
-        return redirect(url_for('users'))
-    else:
-        session['update'] = id
-        return render_template('update.html', data=data)
+        if len(data) == 0:
+            return redirect(url_for('users'))
+        else:
+            session['update'] = id
+        return render_template('update.html', data = data, session_user_name=username_session)
+
+    return redirect(url_for('login'))
 
 
 @app.route('/updateuser', methods=['POST'])
@@ -99,7 +172,10 @@ def updateuser():
 def settings():
     data = db.read_settings(None)
 
-    return render_template('/index_s.html', data = data)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('/index_s.html', session_user_name=username_session, data = data)
+    return redirect(url_for('login'))
 
 
 @app.route('/updates/<int:id>/')
@@ -135,21 +211,30 @@ def updatesettings():
 def orders():
     data = db.read_orders(None)
 
-    return render_template('/index_o.html', data = data)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('/index_o.html', session_user_name=username_session, data = data)
+    return redirect(url_for('login'))
 
 
 @app.route('/closedorders')
 def closedorders():
     data = db.read_corders(None)
 
-    return render_template('/index_co.html', data = data)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('/index_co.html', session_user_name=username_session, data = data)
+    return redirect(url_for('login'))
 
 
 @app.route('/logs')
 def logs():
     data = db.read_logs(None)
 
-    return render_template('/logs.html', data = data)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('/logs.html', session_user_name=username_session, data = data)
+    return redirect(url_for('login'))
 
 ####
 
@@ -158,19 +243,24 @@ def logs():
 def markets():
     data = db.read_markets(None)
 
-    return render_template('/index_market.html', data = data)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('/index_market.html', session_user_name=username_session, data = data)
+    return redirect(url_for('login'))
 
 
 @app.route('/updatem/<int:id>/')
 def updatem(id):
     data = db.read_markets(id)
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        if len(data) == 0:
+            return redirect(url_for('markets'))
+        else:
+            session['update'] = id
+        return render_template('update_market.html', data = data, session_user_name=username_session)
 
-    if len(data) == 0:
-        return redirect(url_for('markets'))
-    else:
-        session['update'] = id
-        return render_template('update_market.html', data=data)
-
+    return redirect(url_for('login'))
 
 @app.route('/updatemarket', methods=['POST'])
 def updatemarket():
@@ -191,12 +281,15 @@ def updatemarket():
 @app.route('/deletem/<int:id>/')
 def deletem(id):
         data = db.read_markets(id)
+        if 'username' in session:
+            username_session = escape(session['username']).capitalize()
+            if len(data) == 0:
+                return redirect(url_for('markets'))
+            else:
+                session['delete'] = id
+            return render_template('delete_market.html', data=data, session_user_name=username_session)
 
-        if len(data) == 0:
-            return redirect(url_for('markets'))
-        else:
-            session['delete'] = id
-            return render_template('delete_market.html', data=data)
+        return redirect(url_for('login'))
 
 @app.route('/deletemarket', methods=['POST'])
 def deletemarket():
@@ -216,7 +309,10 @@ def deletemarket():
 
 @app.route('/addm/')
 def addm():
-            return render_template('/add_market.html')
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('add_market.html', session_user_name=username_session)
+    return redirect(url_for('login'))
 
 
 
