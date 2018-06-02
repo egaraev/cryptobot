06956c69bf7a41f8a2376036443789bd17b89f68
egaraev@gmail.com <Kax0vka44609>
2018-06-02 14:21:42 +0200

    02.06.2018 Stop Bot added. Profit depends on HA trends

--- neuralprediction.py
+++ neuralprediction.py
@@ -85,7 +85,7 @@ def learn():
             #print market, currtime, prediction_info(market)[1]
             #print market, prediction_info(market)[2]
 
-            if prediction_info(market)[2] == 'UP':# and (int(currtime) - int(prediction_info(market)[1])) >= 3600:
+            if prediction_info(market)[2] == 'UP' and (int(currtime) - int(prediction_info(market)[1])) >= 3600:
 
                 #print market, current_price, prediction_info(market)[0], prediction_info(market)[1], prediction_info(market)[2]
                 if (current_price >= prediction_info(market)[0]  or (int(currtime) - int(prediction_info(market)[1])) >= 28800): #21600
@@ -264,7 +264,7 @@ def learn():
                     finally:
                         db.close()
 
-            elif prediction_info(market)[2] == 'DOWN':# and (int(currtime) - int(prediction_info(market)[1])) >= 3600:
+            elif prediction_info(market)[2] == 'DOWN' and (int(currtime) - int(prediction_info(market)[1])) >= 3600:
                 if (current_price <= prediction_info(market)[0] or (int(currtime) - int(prediction_info(market)[1])) >= 28800):  #21600
                     print market, 'Running learning again D', current_price
 
