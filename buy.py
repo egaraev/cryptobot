6bd81ef9601cfb52e29ba943dc7829687120823e
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-05-07 21:59:25 +0200

    07.05.2019 - added clear ha to buy.py

--- buy.py
+++ buy.py
@@ -112,7 +112,10 @@ def tick():
                     buy_count = str(heikin_ashi(market, 42))
                     sell_summ = str(heikin_ashi(market, 41))
                     sell_count = str(heikin_ashi(market, 43))
-
+                   
+                    hah_trend=heikin_ashi(market, 47)
+                    ha_trend=heikin_ashi(market, 48)
+                    had_trend = heikin_ashi(market, 49)
 
 
 
@@ -259,7 +262,7 @@ def tick():
                                         'insert into logs(date, log_entry) values("%s", "%s")' % (currenttime, printed))
                                     cursor.execute(
                                         'insert into orders(market, quantity, price, active, date, timestamp, iteration, btc_direction, params, heikin_ashi) values("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")' % (
-                                        market, buy_quantity, newask, "1", currenttime, timestamp, "1", btc_trend, '  BTC: ' + str(btc_trend) + '  HAD: ' + str(HAD_trend) + ' HA: ' + str(HA_trend) + ' HAH: ' + str(HAH_trend) + '  %  ' + str(percent_sql) + '  vol  ' + str(volume_sql)  + ' HC: ' + str(hour) + ' 30mC: ' + str(thirtymin) + ' 5mC: ' + str(fivemin)+' CS '+str(candles_signal_short) +' '+str(candles_signal_long) + '  AI   ' + str(ai_prediction(market)) + ' Score: ' + str(score) + ' Score trend ' + str(score_trend) + ' Pos.tweets: '+str(positive)+ ' Neg.tweets: '+str(negative)+ ' Buy.summ: '+str(buy_summ)+ ' Buy.count: '+str(buy_count)+ ' Sell.summ: '+str(sell_summ)+ ' Sell.count: '+str(sell_count),
+                                        market, buy_quantity, newask, "1", currenttime, timestamp, "1", btc_trend, '  BTC: ' + str(btc_trend) + '  HAD: ' + str(HAD_trend) + ' HA: ' + str(HA_trend) + ' HAH: ' + str(HAH_trend)  + '  had: ' + str(had_trend) + ' ha: ' + str(ha_trend) + ' hah: ' + str(hah_trend)   + '  %  ' + str(percent_sql) + '  vol  ' + str(volume_sql)  + ' HC: ' + str(hour) + ' 30mC: ' + str(thirtymin) + ' 5mC: ' + str(fivemin)+' CS '+str(candles_signal_short) +' '+str(candles_signal_long) + '  AI   ' + str(ai_prediction(market)) + ' Score: ' + str(score) + ' Score trend ' + str(score_trend) + ' Pos.tweets: '+str(positive)+ ' Neg.tweets: '+str(negative)+ ' Buy.summ: '+str(buy_summ)+ ' Buy.count: '+str(buy_count)+ ' Sell.summ: '+str(sell_summ)+ ' Sell.count: '+str(sell_count),
                                         HA_trend))
                                     cursor.execute("update orders set serf = %s, one_step_active =1 where market = %s and active =1",
                                                    (serf, market))
