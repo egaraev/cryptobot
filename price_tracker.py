378f076f117be01a307a5974ded42e63601155ce
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-09-08 12:15:06 +0200

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -36,40 +36,41 @@ def tick():
                 market = summary['MarketName']
                 last = float(summary['Last'])  # last price
                 print "Beginning of the minute: ", last
-                time.sleep(40)
+                time.sleep(100)
                 
             if available_market_list(summary['MarketName']):
+                market_summ = c.get_market_summaries().json()['result']
                 market = summary['MarketName']
                 last = float(summary['Last'])  # last price                
                 print "First 10 seconds of the minute: ", last
-                time.sleep(40)
+                time.sleep(100)
            
 
             if available_market_list(summary['MarketName']):
                 market = summary['MarketName']
                 last = float(summary['Last'])  # last price  
                 print "First 20 seconds of the minute: ", last
-                time.sleep(40)
+                time.sleep(100)
                 
             if available_market_list(summary['MarketName']):
                 market = summary['MarketName']
                 last = float(summary['Last'])  # last price  
                 print "First 30 seconds of the minute: ", last
-                time.sleep(40)
+                time.sleep(100)
                 
                 
             if available_market_list(summary['MarketName']):
                 market = summary['MarketName']
                 last = float(summary['Last'])  # last price  
                 print "First 40 seconds of the minute: ", last
-                time.sleep(40)
+                time.sleep(100)
                 
                 
             if available_market_list(summary['MarketName']):
                 market = summary['MarketName']
                 last = float(summary['Last'])  # last price  
                 print "First 50 seconds of the minute: ", last
-                time.sleep(40)
+                time.sleep(100)
                 
             if available_market_list(summary['MarketName']):
                 market = summary['MarketName']
