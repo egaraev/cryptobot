b090d71f16bc3e75ea93d85f967d9d9908fd4979
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-09-15 12:39:13 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -319,3 +319,16 @@ fi
      /usr/bin/python2.7 grow_percent.py &
 
  fi
+
+
+ SERVICE24='price_tracker.py'
+
+ if ps ax | grep -v grep | grep $SERVICE24 > /dev/null
+ then
+     echo "$SERVICE24 service running "
+ else
+     echo there is no such "$SERVICE24 service, starting"
+     cd /root/PycharmProjects/cryptobot
+     /usr/bin/python2.7 price_tracker.py &
+
+ fi
