4a53742eb2b23f321aca3ca8c4d87b12aaec5c06
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-09-15 10:20:17 +0200

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -36,9 +36,10 @@ def tick():
             if available_market_list(summary['MarketName']):
                 market_summ = c.get_market_summaries().json()['result']
                 market = summary['MarketName']
-                last = float(summary['Last'])  # last price                
+                last = float(summary['Last'])  # last price 
+                test= get_prices(market, 2)
                 print "First 10 seconds of the minute: ", last
-                print get_prices(market, 2)
+                print test
                 time.sleep(10)
                 print currtime
                 try:
