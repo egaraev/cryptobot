20e45e1946abffac5fb68ef3a8f0a044352c94c8
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-09-16 23:57:38 +0200

    Update start_price.sh

--- start_price.sh
+++ start_price.sh
@@ -3,10 +3,11 @@
 
  if ps ax | grep -v grep | grep $SERVICE24 > /dev/null
  then
-     echo "$SERVICE24 service running "
+     #echo "$SERVICE24 service running "
+     break
  else
-     echo there is no such "$SERVICE24 service, starting"
+     #echo there is no such "$SERVICE24 service, starting"
      cd /root/PycharmProjects/cryptobot
-     stdbuf -oL /usr/bin/python2.7 price_tracker.py >> /root/price_tracker
+     /usr/bin/python2.7 price_tracker.py &
 
  fi
