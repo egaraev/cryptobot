2faefe9e50ae93fc9cb7aaa3f18f324d67ec53a0
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-09-16 23:38:34 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -329,6 +329,6 @@ fi
  else
      echo there is no such "$SERVICE24 service, starting"
      cd /root/PycharmProjects/cryptobot
-     /usr/bin/python2.7 price_tracker.py &> /root/price_tracker
+     stdbuf -oL /usr/bin/python2.7 price_tracker.py >> /root/price_tracker
 
  fi
