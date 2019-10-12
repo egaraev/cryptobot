6f6ed6250b757c5e8f898bbe870df754975c7a18
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-10-12 09:40:19 +0200

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -23,6 +23,7 @@ def SUMM():
     open('data/out16.csv', 'w').close()
     open('data/out17.csv', 'w').close()
     open('data/out-max.csv', 'w').close()
+    print('all files opened')
 
 
 #    try:
@@ -46,68 +47,87 @@ def SUMM():
 
     for summary in market_summ: #Loop trough the market summary
             if available_market_list(summary['MarketName']):
+                print('Starting loop')
                 market = summary['MarketName']
                 max_loss=aftercount_serf(market)-summ_serf(market)
                 f= open('data/out8_tmp.csv', 'a')
+                print('Open data/out8_tmp.csv')
                 print >> f, str(market)
                 if (aftercount_serf_sell(market, 0) - summ_serf_sell(market, 0))>0:
                     sell_loss_0 = aftercount_serf_sell(market, 0) - summ_serf_sell(market, 0)
                     f3 = open('data/out11_tmp.csv', 'a')
+                    print('1')
                     print >> f3, sell_loss_0
                 else:
                     f3 = open('data/out11_tmp.csv', 'a')
+                    print('2')
                     print >> f3, 0.0
                 if (aftercount_serf_sell(market, 1) - summ_serf_sell(market, 1))>0:
                     sell_loss_1 = aftercount_serf_sell(market, 1) - summ_serf_sell(market, 1)
                     f4 = open('data/out12_tmp.csv', 'a')
+                    print('3')
                     print >> f4, sell_loss_1
                 else:
                     f4 = open('data/out12_tmp.csv', 'a')
+                    print('4')
                     print >> f4, 0.0
                 if (aftercount_serf_sell(market, 2) - summ_serf_sell(market, 2))>0:
                     sell_loss_2 = aftercount_serf_sell(market, 2) - summ_serf_sell(market, 2)
                     f5 = open('data/out13_tmp.csv', 'a')
+                    print('5')
                     print >> f5, sell_loss_2
                 else:
                     f5 = open('data/out13_tmp.csv', 'a')
+                    print('6')
                     print >> f5, 0.0
                 if (aftercount_serf_sell(market, 3) - summ_serf_sell(market, 3))>0:
                     sell_loss_3 = aftercount_serf_sell(market, 3) - summ_serf_sell(market, 3)
                     f2 = open('data/out10_tmp.csv', 'a')
+                    print('7')
                     print >> f2, sell_loss_3
                 else:
                     f2 = open('data/out10_tmp.csv', 'a')
+                    print('8')
                     print >> f2, 0.0
                 if (aftercount_serf_sell(market, 4) - summ_serf_sell(market, 4))>0:
                     sell_loss_4 = aftercount_serf_sell(market, 4) - summ_serf_sell(market, 4)
                     f6 = open('data/out14_tmp.csv', 'a')
+                    print('9')
                     print >> f6, sell_loss_4
                 else:
                     f6 = open('data/out14_tmp.csv', 'a')
+                    print('10')
                     print >> f6, 0.0
                 if (aftercount_serf_sell(market, 5) - summ_serf_sell(market, 5))>0:
                     sell_loss_5 = aftercount_serf_sell(market, 5) - summ_serf_sell(market, 5)
                     f7 = open('data/out15_tmp.csv', 'a')
+                    print('11')
                     print >> f7, sell_loss_5
                 else:
                     f7 = open('data/out15_tmp.csv', 'a')
+                    print('12')
                     print >> f7, 0.0
                 if (aftercount_serf_sell(market, 6) - summ_serf_sell(market, 6))>0:
                     sell_loss_6 = aftercount_serf_sell(market, 6) - summ_serf_sell(market, 6)
                     f8 = open('data/out16_tmp.csv', 'a')
+                    print('13')
                     print >> f8, sell_loss_6
                 else:
                     f8 = open('data/out16_tmp.csv', 'a')
+                    print('14')
                     print >> f8, 0.0
                 if (aftercount_serf_sell(market, 7) - summ_serf_sell(market, 7))>0:
                     sell_loss_7 = aftercount_serf_sell(market, 7) - summ_serf_sell(market, 7)   # was 8, changed to 7
                     f9 = open('data/out17_tmp.csv', 'a')
+                    print('15')
                     print >> f9, sell_loss_7
                 else:
                     f9 = open('data/out17_tmp.csv', 'a')
+                    print('16')
                     print >> f9, 0.0
 
                 f10 = open('data/out-max_tmp.csv', 'a')
+                print('17')
                 print >> f10, max_loss
 
     os.rename('data/out8_tmp.csv', 'data/out8.csv')
@@ -120,6 +140,7 @@ def SUMM():
     os.rename('data/out16_tmp.csv', 'data/out16.csv')
     os.rename('data/out17_tmp.csv', 'data/out17.csv')
     os.rename('data/out-max_tmp.csv', 'data/out-max.csv')
+    print('Files renamed')
 
 
 
