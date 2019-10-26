1b2e8d2ec18996d69289ac6c941bf6edb1ec170f
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-10-26 15:21:49 +0200

    Update sell.py

--- sell.py
+++ sell.py
@@ -206,21 +206,21 @@ def tick():
 
                     
                     ### ============== temporary disabled history
-#                    orderid = status_orders(market, 0)
-#                    history=""
-#                    print('Updating history for ' + market)
-#                    try:
-#                        db = MySQLdb.connect("database-service", "cryptouser", "123456", "cryptodb")
-#                        cursor = db.cursor()
-#                        cursor.execute('SELECT GROUP_CONCAT(signals) FROM orderlogs where orderid=("%s")' % orderid)
-#                        history = cursor.fetchone()
-#                        cursor.execute('update orders set history=%s where order_id=%s',(history, orderid))
-#                        db.commit()
-#                    except MySQLdb.Error, e:
-#                        print "Error %d: %s" % (e.args[0], e.args[1])
-#                        sys.exit(1)
-#                    finally:
-#                        db.close()
+                    orderid = status_orders(market, 0)
+                    history=""
+                    print('Updating history for ' + market)
+                    try:
+                        db = MySQLdb.connect("database-service", "cryptouser", "123456", "cryptodb")
+                        cursor = db.cursor()
+                        cursor.execute('SELECT GROUP_CONCAT(signals) FROM orderlogs where orderid=("%s")' % orderid)
+                        history = cursor.fetchone()
+                        cursor.execute('update orders set history=%s where order_id=%s',(history, orderid))
+                        db.commit()
+                    except MySQLdb.Error, e:
+                        print "Error %d: %s" % (e.args[0], e.args[1])
+                        sys.exit(1)
+                    finally:
+                        db.close()
                     ### ============== temporary disabled history
 
     # Force Stop
