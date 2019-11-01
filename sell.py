31937f7b1c3640c29b70eedc256d2b183633c2f3
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2019-11-01 10:54:54 +0100

    Update sell.py

--- sell.py
+++ sell.py
@@ -286,11 +286,11 @@ def tick():
                                 #print ('   6 -Selling ' + str(format_float(sell_quantity_sql)) + ' units of ' + market + ' for ' + str(format_float(newbid)) + '  and getting  +' + str(format_float(ask * bought_quantity_sql - bought_price_sql * bought_quantity_sql)) + ' BTC' + ' or ' + str(format_float((newbid * bought_quantity_sql - bought_price_sql * bought_quantity_sql) * BTC_price)) + ' USD'   + ' and ' + procent_serf +'  %')
                                 try:
                                     print "Connecting to Mysql to perform sell for reason 4"
-                                    printed = ('   6 -Selling ' + str(format_float(
-                                        sell_quantity_sql)) + ' units of ' + market + ' for ' + str(
-                                        format_float(newbid)) + '  and getting  +' + str(format_float(
-                                        ask * bought_quantity_sql - bought_price_sql * bought_quantity_sql)) + ' BTC' + ' or ' + str(
-                                        format_float((newbid * bought_quantity_sql - bought_price_sql * bought_quantity_sql) * BTC_price)) + ' USD'   + ' and ' + procent_serf +'  %')
+                                    #printed = ('   6 -Selling ' + str(format_float(
+                                    #    sell_quantity_sql)) + ' units of ' + market + ' for ' + str(
+                                    #    format_float(newbid)) + '  and getting  +' + str(format_float(
+                                    #    ask * bought_quantity_sql - bought_price_sql * bought_quantity_sql)) + ' BTC' + ' or ' + str(
+                                    #    format_float((newbid * bought_quantity_sql - bought_price_sql * bought_quantity_sql) * BTC_price)) + ' USD'   + ' and ' + procent_serf +'  %')
                                     print "Connecting to Mysql to perform sell for reason 4.1"
                                     db = MySQLdb.connect("database-service", "cryptouser", "1234567", "cryptodb")
                                     print "Connecting to Mysql to perform sell for reason 4.2"
